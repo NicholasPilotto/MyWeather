1f5ced44ef2086c9646a0ec83eaeb4ff30af1bbb
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-11-13 20:36:18 +0100

    New model data

--- MyWeather/Model/WeatherModel.swift
+++ MyWeather/Model/WeatherModel.swift
@@ -12,12 +12,21 @@ struct WeatherModel: Codable {
     let longitude: Double
     let elevation: Double
     let hourlyUnits: Units
-    let data: WeatherData
+    let hourly : WeatherData
+    let daily: Daily
+}
+
+struct CurrentWeather: Codable {
+    let temperature: Double
+    let windspeed: Double
+    let weathercode: Int
 }
 
 struct Units: Codable {
-    let time: String
+    let time: [String]
     let temperature2m: String
+    let relaviveHumidity2m: String
+    let precipitation: String
     let rain: String
     let windspeed10m: String
 }
@@ -25,6 +34,15 @@ struct Units: Codable {
 struct WeatherData: Codable {
     let time: [String]
     let temperature2m: [Double]
+    let relativeHumidity2m: [Int]
+    let precipitation: [Double]
     let rain: [Double]
     let windspeed10m: [Double]
+}
+
+struct Daily: Codable {
+    let temperature2mMax: [Double]
+    let temperature2mMin: [Double]
+    let sunrise: [Date]
+    let sunset: [Date]
 }
