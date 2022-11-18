b4e4c764f7e777f8e9c2e9570b9fd68cd7f57ee3
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-11-18 15:00:34 +0100

    MainViewViewModel

---
+++ MyWeather/ViewModel/MainViewViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewViewModel.swift
+//  MyWeather
+//
+//  Created by Nicholas Pilotto on 18/11/22.
+//
+
+import Foundation
+
+struct MainViewViewModel {
+    let city: String
+    let weatherImage: String
+    let temperature: Double
+    let temperatureUnit: String
+    let weather: String
+    let date: String
+    let info: [MainViewInfo]
+}
+
+struct MainViewInfo {
+    let infoIcon: String
+    let infoValue: Double
+    let valueUnit: String
+    let infoName: String
+}
